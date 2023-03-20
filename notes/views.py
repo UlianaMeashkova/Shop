ea392cfe21bf43e6fd5286232a71e935dc663bcf
UlianaMeashkova <el.mahno@gmail.com>
2023-03-20 21:09:17 +0300

    new html & forms

--- notes/views.py
+++ notes/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
-from django.http import HttpResponse
+from notes.forms import AddNoteForm
 
 
 def index(request):
-    return HttpResponse("Notes index view")
+    notes = []
+    if request.method == "POST":
+        form = AddNoteForm(request.POST)
+        if form.is_valid():
+            # Create new DB record
+            pass
+    else:
+        form = AddNoteForm()
+    return render(request, "index.html", {"notes": notes, "form": form})
 
